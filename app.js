1b3b5c428e80ab94ed72cbcef1a8eebe09c96e65
nilay1607 <shah.nilay@tcs.com>
2019-11-03 16:07:46 +0530

    removed unwanted code

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ const fs = require('fs');
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
-const multer = require('multer');
 const helmet = require('helmet');
 const compression = require('compression');
 const morgon = require('morgan');
@@ -14,27 +13,6 @@ const authRoutes = require('./routes/auth');
 
 const app = express();
 
-const fileStorage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, 'images');
-  },
-  filename: (req, file, cb) => {
-    cb(null, new Date().toISOString() + '-' + file.originalname);
-  }
-});
-
-const fileFilter = (req, file, cb) => {
-  if (
-    file.mimetype === 'image/png' ||
-    file.mimetype === 'image/jpg' ||
-    file.mimetype === 'image/jpeg'
-  ) {
-    cb(null, true);
-  } else {
-    cb(null, false);
-  }
-};
-
 const accessLogStream = fs.createWriteStream(path.join(__dirname,'access.log'),
 {flags: 'a'}
 )
@@ -43,12 +21,7 @@ app.use(helmet());
 app.use(compression());
 app.use(morgon('combined',{ stream:accessLogStream }));
 
-// app.use(bodyParser.urlencoded()); // x-www-form-urlencoded <form>
-app.use(bodyParser.json()); // application/json
-app.use(
-  multer({ storage: fileStorage, fileFilter: fileFilter }).single('image')
-);
-app.use('/images', express.static(path.join(__dirname, 'images')));
+app.use(bodyParser.json()); 
 
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
@@ -82,4 +55,3 @@ mongoose
   .catch(err => {
     console.log(err);
   });
-
--- controllers/feed.js
+++ controllers/feed.js
@@ -40,11 +40,6 @@ exports.createPost = (req, res, next) => {
     error.statusCode = 422;
     throw error;
   }
-  // if (!req.file) {
-  //   const error = new Error('No image provided.');
-  //   error.statusCode = 422;
-  //   throw error;
-  // }
   const imageUrl = "images/Desert.jpg";
   const title = req.body.title;
   const content = req.body.content;
@@ -110,14 +105,6 @@ exports.updatePost = (req, res, next) => {
   const title = req.body.title;
   const content = req.body.content;
   const imageUrl = "images/Desert.jpg";
-  // if (req.file) {
-  //   imageUrl = req.file.path;
-  // }
-  // if (!imageUrl) {
-  //   const error = new Error('No file picked.');
-  //   error.statusCode = 422;
-  //   throw error;
-  // }
   Post.findById(postId)
     .then(post => {
       if (!post) {
